00c1875752ca53904b89ee1d2a3e13400aabb56b
Popoola Temilorun <horluwatemilorunolamilekan@gmail.com>
2024-12-26 21:14:18 +0100

    admin(customuser):change the fields on the CustomAdmin to include all fields because of the permissionmixin

--- users/admin.py
+++ users/admin.py
@@ -11,10 +11,6 @@ CustomUser = get_user_model()
 @admin.register(CustomUser)
 class CustomUserAdmin(admin.ModelAdmin):
 
-    # fields specified will be used to capture details to be stored
-    fields = ('username', 'email', 'full_name', 'avatar', 'bio', 'is_online', 'last_seen', 'device_tokens', 'role',
-              'email_verified', 'verification_token', 'verification_token_created', 'date_joined', 'is_active', 'is_staff')
-
     list_display = ('id', 'username', 'email', 'email_verified',
                     'full_name', 'is_staff', 'is_online', 'last_seen')
     list_editable = ('username', 'email',
@@ -24,6 +20,10 @@ class CustomUserAdmin(admin.ModelAdmin):
 
     # any words entered into the search box will be searched from this fields e.g username ='what was entered into search box'
     search_fields = ('username', 'full_name', 'device_tokens')
+
+    class Meta:
+        model = CustomUser
+        fields = "__all__"
 
 
 @admin.register(Role)
