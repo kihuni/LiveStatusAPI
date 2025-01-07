e37e528cf69c01db54bc59ea718f5f6077ee88d4
Popoola Temilorun <horluwatemilorunolamilekan@gmail.com>
2025-01-07 19:23:15 +0100

    refactor(Custom User Admin): exclude redundant field that django use to handle permission since it will by Role model

--- users/admin.py
+++ users/admin.py
@@ -11,19 +11,20 @@ CustomUser = get_user_model()
 @admin.register(CustomUser)
 class CustomUserAdmin(admin.ModelAdmin):
 
+    exclude = ('groups','user_permissions') # django uses this fields for permissions but we are using roles
     list_display = ('id', 'username', 'email', 'email_verified',
                     'full_name', 'is_staff', 'is_online', 'last_seen')
     list_editable = ('username', 'email',
                      'full_name', 'is_online', 'is_staff', 'is_staff')
     list_filter = ('username', 'email')
     list_per_page = 10  # applies pagination
+    date_hierarchy = 'date_joined'
 
     # any words entered into the search box will be searched from this fields e.g username ='what was entered into search box'
     search_fields = ('username', 'full_name', 'device_tokens')
 
     class Meta:
         model = CustomUser
-        fields = "__all__"
 
 
 @admin.register(Role)
